402c9052df96914004456f80f02b39a181782a3a
osdal <osdal67@google.com>
2022-10-07 12:15:10 +0300

    scopes and endpoints

--- lib/auth/infrastructure/github_authenticator.dart
+++ lib/auth/infrastructure/github_authenticator.dart
@@ -7,6 +7,17 @@ class GithubAuthenticator {
 
   GithubAuthenticator(this._credentialStorage);
 
+  static const clientId = '2bb316fa8b0c688fcc6b';
+  static const clientSecret = '19bff2d234b7c588f17dc026341bfcc76ae91e4a';
+  static const scopes = ['read:user', 'repo'];
+  static final authorizationEndpoint =
+      Uri.parse('https://github.com/login/oauth/authorize');
+
+  static final tokenEndpoint =
+      Uri.parse('https://github.com/login/oauth/access_token');
+
+  static final redirectUrl = Uri.parse('http://localhost:300/callback');
+
   Future<Credentials?> getSignedInCredentials() async {
     try {
       final storedCredentials = await _credentialStorage.read();
